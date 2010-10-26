6928168efc33f1fd43115bcefbe4f448ae54155c
mordaroso <mordaroso@gmail.com>
2010-10-26 18:23:59 +0200

    add support for passenger standalone

--- lib/guard/passenger.rb
+++ lib/guard/passenger.rb
@@ -4,18 +4,34 @@ require 'guard/guard'
 module Guard
   class Passenger < Guard
 
+
     # ================
     # = Guard method =
     # ================
 
+    def initialize(watchers = [], options = {})
+      super
+      @standalone = options[:standalone].nil? ? false : options[:standalone]
+      @port = options[:port].nil? ? 3000 : options[:port]
+    end
+
     # Call once when guard starts
     def start
       UI.info "Guard::Passenger is guarding your changes!"
+      if standalone?
+        start_passenger
+      else
+        true
+      end
     end
 
     # Call with Ctrl-C signal (when Guard quit)
     def stop
-      true
+      if standalone?
+        stop_passenger
+      else
+        true
+      end
     end
 
     # Call with Ctrl-Z signal
@@ -33,6 +49,14 @@ module Guard
       restart_passenger
     end
 
+    def standalone?
+      @standalone
+    end
+
+    def port
+      @port
+    end
+
     private
       def restart_passenger
         result = system 'touch tmp/restart.txt'
@@ -43,6 +67,24 @@ module Guard
         end
 
         result
+      end
+
+      def start_passenger
+        result = system "passenger start -p #{port} -d"
+        if result
+          UI.info "Passenger standalone startet at port #{port}"
+        else
+          UI.info "Passenger standalone failed to start at port #{port}"
+        end
+        result
+      end
+
+      def stop_passenger
+        result = system 'passenger stop'
+        if result
+          UI.info "Passenger standalone stopeed"
+        end
+        true
       end
 
   end
--- spec/passenger/passenger_spec.rb
+++ spec/passenger/passenger_spec.rb
@@ -3,6 +3,69 @@ require 'spec_helper'
 describe Guard::Passenger do
   subject { Guard::Passenger.new }
 
+  describe 'options' do
+
+    context 'standalone' do
+      it 'should be false by default' do
+        subject.should_not be_standalone
+      end
+
+      it 'should be set to true' do
+        subject = Guard::Passenger.new([], {:standalone => true})
+        subject.should be_standalone
+      end
+    end
+
+    context 'port' do
+      it 'should be 3000 by default' do
+        subject.port.should be 3000
+      end
+
+      it 'should be set to 1337' do
+        subject = Guard::Passenger.new([], {:port => 1337})
+        subject.port.should be 1337
+      end
+    end
+
+  end
+
+  context 'start' do
+
+    it 'should not call `passenger start\' command' do
+      subject.should_not_receive(:system).with('passenger start -p 3000 -d')
+      subject.start
+    end
+
+    it 'should call `passenger start\' command if standalone is set' do
+      subject.should_receive(:standalone?).and_return(true)
+      subject.should_receive(:system).with('passenger start -p 3000 -d')
+      subject.start
+    end
+
+    it 'should call `passenger start -p 1337\' command if standalone is set and port is set to 1337' do
+      subject.should_receive(:standalone?).and_return(true)
+      subject.should_receive(:port).exactly(2).and_return(1337)
+      subject.should_receive(:system).with('passenger start -p 1337 -d')
+      subject.start
+    end
+
+  end
+
+  context 'stop' do
+
+    it 'should not call `passenger stop\' command' do
+      subject.should_not_receive(:system).with('passenger stop')
+      subject.stop
+    end
+
+    it 'should call `passenger stop\' command if standalone is set' do
+      subject.should_receive(:standalone?).and_return(true)
+      subject.should_receive(:system).with('passenger stop')
+      subject.stop
+    end
+
+  end
+
   context 'reload' do
 
     it 'should call `touch tmp/restart.txt\' command' do
