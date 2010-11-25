2c35f4f4de4f7b0f0eabd1f47eee10887325faf3
Rémy Coutable <rymai@rymai.com>
2010-11-25 20:59:40 +0100

    Cleaned up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require 'rubygems'
 require 'guard/passenger'
 require 'rspec'
 
+ENV["GUARD_ENV"] = 'test'
+
 Dir["#{File.expand_path('..', __FILE__)}/support/**/*.rb"].each { |f| require f }
 
 puts "Please do not update/create files while tests are running."
@@ -10,16 +12,6 @@ RSpec.configure do |config|
   config.color_enabled = true
   config.filter_run :focus => true
   config.run_all_when_everything_filtered = true
-  
-  config.before(:each) do
-    ENV["GUARD_ENV"] = 'test'
-    @fixture_path = Pathname.new(File.expand_path('../fixtures/', __FILE__))
-  end
-  
-  config.after(:each) do
-    ENV["GUARD_ENV"] = nil
-  end
-  
 end
 
 # Thanks to Jonas Pfenniger for this!
